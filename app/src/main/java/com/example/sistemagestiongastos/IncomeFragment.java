8b3fa67ed9e3389c521c173f4007271be4c73744
Chris Apaza <chrisalanapazaa@gmail.com>
2022-09-19 22:25:50 -0400

    Finished Incomes UI

--- app/src/main/java/com/example/sistemagestiongastos/IncomeFragment.java
+++ app/src/main/java/com/example/sistemagestiongastos/IncomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.sistemagestiongastos;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +10,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import newadapter.SpinnerNewAdapter;
 
@@ -19,6 +30,15 @@ public class IncomeFragment extends Fragment {
             R.drawable.piggy_bank};
     Spinner spinner;
     SpinnerNewAdapter adapter;
+
+    String[] textArrayCat = {"Prestamo", "Salario", "Ventas"};
+    Integer[] imageArrayCat = {R.drawable.loan, R.drawable.salary,
+            R.drawable.sales};
+    Spinner spinnerCat;
+    SpinnerNewAdapter adapterCat;
+
+    EditText etDate;
+    EditText etTime;
 
     public IncomeFragment() {
         // Required empty public constructor
@@ -37,8 +57,58 @@ public class IncomeFragment extends Fragment {
         } else {
             index = 0;
         }
-        Log.d("index", String.valueOf(index));
         spinner.setSelection(index);
+        spinnerCat = view.findViewById(R.id.spcategoriaingresos);
+        adapterCat = new SpinnerNewAdapter(getContext(), R.layout.spinner_value_layout, textArrayCat, imageArrayCat);
+        spinnerCat.setAdapter(adapterCat);
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        etDate = view.findViewById(R.id.etfechaingreso);
+        etDate.setText(day + "/" + (month + 1) + "/" + year);
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        etTime = view.findViewById(R.id.ethoraingreso);
+        etTime.setText(hour + ":" + minute);
+        etTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
         return view;
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerFragment newFragment = TimePickerFragment.newInstance(new TimePickerDialog.OnTimeSetListener() {
+
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                String time = hourOfDay + ":" + minute;
+                etTime.setText(time);
+            }
+        });
+        newFragment.show(getChildFragmentManager(), "timePicker");
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                etDate.setText(selectedDate);
+            }
+        });
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
     }
 }
