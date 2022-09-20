26056729d9d317e34301a0e6629eaad3aa51d88a
Chris Apaza <chrisalanapazaa@gmail.com>
2022-09-20 00:09:18 -0400

    Finished UI

--- app/src/main/java/com/example/sistemagestiongastos/ListaMovimientos.java
+++ app/src/main/java/com/example/sistemagestiongastos/ListaMovimientos.java
@@ -7,58 +7,54 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListaMovimientos#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
+import newadapter.SpinnerNewAdapter;
+
 public class ListaMovimientos extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String[] textArray = {"Tarjeta", "Efectivo", "Ahorros"};
+    Integer[] imageArray = {R.drawable.credit_card, R.drawable.money, R.drawable.piggy_bank};
+    Spinner spinner;
+    SpinnerNewAdapter adapter;
+
+    Spinner spmes, spanio;
+    ArrayAdapter<String> adaptermes, adapteranio;
+    String[] mes = new String[]{"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    String[] anio = new String[]{"2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"};
 
     public ListaMovimientos() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListaMovimientos.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListaMovimientos newInstance(String param1, String param2) {
-        ListaMovimientos fragment = new ListaMovimientos();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_movimientos, container, false);
+        View view = inflater.inflate(R.layout.fragment_lista_movimientos, container, false);
+
+        spinner = view.findViewById(R.id.spfuentelista);
+        adapter = new SpinnerNewAdapter(getContext(), R.layout.spinner_value_layout, textArray, imageArray);
+        spinner.setAdapter(adapter);
+
+
+        spmes = view.findViewById(R.id.spmes);
+        spanio = view.findViewById(R.id.spanio);
+        adaptermes = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, mes);
+        adapteranio = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, anio);
+        spmes.setAdapter(adaptermes);
+        spanio.setAdapter(adapteranio);
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+
+        spmes.setSelection(month);
+        spanio.setSelection(year-2020);
+        return view;
     }
 }
